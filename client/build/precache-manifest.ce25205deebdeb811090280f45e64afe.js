8cf093f80b6dd7e30bac731daf0c1fd9a4c86999
padmanadella2016 <52078938+padmanadella2016@users.noreply.github.com>
2020-02-02 09:17:08 +1100

    'second'

---
+++ client/build/precache-manifest.ce25205deebdeb811090280f45e64afe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0f6f5d66f229d326d102e4de3f6613",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3852939c8ab6d184f2b7",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "43e56cfe3a841317021a",
+    "url": "/static/js/2.9c348327.chunk.js"
+  },
+  {
+    "revision": "a0c18f8786874a74d6fd4d46260562f6",
+    "url": "/static/js/2.9c348327.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3852939c8ab6d184f2b7",
+    "url": "/static/js/main.257c343e.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
